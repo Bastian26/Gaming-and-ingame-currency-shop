1ebf1caab404a13bb6709d812a7a5e9b1520b8cc
Bastian <b.struggl@gmx.de>
2024-03-01 21:04:52 +0100

    added and adjusted WoW Retail site

--- Frontend/src/app/components/wow-retail/wow-retail.component.ts
+++ Frontend/src/app/components/wow-retail/wow-retail.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-wow-retail',
   templateUrl: './wow-retail.component.html',
-  styleUrls: ['./wow-retail.component.scss']
+  styleUrls: ['./wow-retail.component.scss'],
 })
 export class WowRetailComponent implements OnInit {
+  navbarLinks = [
+    { label: 'Home', url: '/home' },
+    { label: 'About', url: '/about' },
+    { label: 'Contact', url: '/contact' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
