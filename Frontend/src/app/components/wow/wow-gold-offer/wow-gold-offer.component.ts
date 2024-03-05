42f61eb003d56f96e4117e7af1dc04f26fafd17d
Bastian <b.struggl@gmx.de>
2024-03-05 19:54:40 +0100

    added first ui of wow gold classic

---
+++ Frontend/src/app/components/wow/wow-gold-offer/wow-gold-offer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wow-gold-offer',
+  templateUrl: './wow-gold-offer.component.html',
+  styleUrls: ['./wow-gold-offer.component.scss']
+})
+export class WowGoldOfferComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
