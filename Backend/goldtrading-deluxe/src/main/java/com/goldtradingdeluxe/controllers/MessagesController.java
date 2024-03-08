f30afc21fd29a7652aee6a2ab3ed024263a49dd1
Bastian <b.struggl@gmx.de>
2024-03-08 22:31:27 +0100

    added liquibase and tested it

--- Backend/goldtrading-deluxe/src/main/java/com/goldtradingdeluxe/controllers/MessagesController.java
+++ Backend/goldtrading-deluxe/src/main/java/com/goldtradingdeluxe/controllers/MessagesController.java
@@ -6,13 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class MessagesController {
 
     @GetMapping("/messages")
     public ResponseEntity<List<String>> messages() {
-        return ResponseEntity.of(Optional.of(Arrays.asList("First", "Second")));
+        return ResponseEntity.ok(Arrays.asList("First", "Second"));
     }
 }
